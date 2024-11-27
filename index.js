e996f90586d953b41d1fc2691540369735af4274
matthewhutchings <matthewhutchings1@gmail.com>
2024-11-27 19:07:36 +0000

    Prepare for Heroku deployment

--- index.js
+++ index.js
@@ -93,12 +93,13 @@ fastify.all('/incoming-call', async (request, reply) => {
         sayMessage = profileInfo.SayMessage;  // Use SayMessage from the API response
     }
 
-    const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
-                          <Response>
-                              <Connect>
-                                  <Stream url="wss://${request.headers.host}/media-stream" />
-                              </Connect>
-                          </Response>`;
+   const websocketHost = process.env.WEBSOCKET_HOST || request.headers.host;
+const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
+                      <Response>
+                          <Connect>
+                              <Stream url="wss://${websocketHost}/media-stream" />
+                          </Connect>
+                      </Response>`;
 
     reply.type('text/xml').send(twimlResponse);
 console.log("sayMessage:" + sayMessage)
