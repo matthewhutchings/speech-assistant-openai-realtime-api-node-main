1e7d5b889feefe0c7282a6008fff1ccb05c67bdc
matthewhutchings <matthewhutchings1@gmail.com>
2024-11-28 00:25:12 +0000

    updated

--- routes/incoming-call.js
+++ routes/incoming-call.js
@@ -22,10 +22,11 @@ export default async function incomingCallRoutes(fastify) {
             const twimlResponse = `
                 <?xml version="1.0" encoding="UTF-8"?>
                 <Response>
-                          <Connect>
-                              <Stream url="wss://${websocketHost}/media-stream" />
-                          </Connect>
-                      </Response>`;
+                    <Connect>
+                        <Stream url="wss://${websocketHost}/media-stream" />
+                    </Connect>
+                </Response>
+            `;
 
             reply.type('text/xml').send(twimlResponse);
             console.log("sayMessage:", sayMessage);
--- routes/websocket.js
+++
@@ -1,8 +0,0 @@
-import { handleWebSocketConnection } from '../utils/websocket-helpers.js';
-
-export default async function websocketRoutes(fastify) {
-    fastify.get('/media-stream', { websocket: true }, (connection, req) => {
-        console.log('WebSocket client connected');
-        handleWebSocketConnection(connection, req);
-    });
-}
